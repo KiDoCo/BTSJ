75e94dd22e7b0dabd55f7b4901a8d7459e8ceb4d
Kiiskinen Mauno TTK17SP <maunokiiskinen@kamk.fi>
2018-09-28 22:32:06 +0300

    Timer slider functionality

---
+++ Assets/Sips/_Mgl_scripts/TimerSlider.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerSlider : MonoBehaviour
+{
+
+    [SerializeField] Slider timer;
+    [SerializeField] float timeLosingSpeed = 0.1f, timeChangeSpeed = 1f;
+    float maxValue;
+
+    void OnEnable()
+    {
+        timer.value = timer.maxValue; // reset
+        maxValue = timer.maxValue;
+    }
+
+    void Update()
+    {
+        timer.value -= timeLosingSpeed * Time.deltaTime;
+
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            AddTime(.5f);
+        }
+        
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            LoseTime(.2f);
+        }
+    }
+
+    public void AddTime(float amount)
+    {
+        float currentValue = timer.value;
+        float wantedValue = currentValue + amount;
+
+        StartCoroutine(ChangeTime(currentValue, wantedValue, timeChangeSpeed));
+        Debug.Log("Gain time");
+    }
+
+    public void LoseTime(float amount)
+    {
+        float currentValue = timer.value;
+        float wantedValue = currentValue - amount;
+
+        StartCoroutine(ChangeTime(currentValue, wantedValue, timeChangeSpeed));
+        Debug.Log("Lost time");
+    }
+
+    IEnumerator ChangeTime(float current, float wanted, float duration)
+    {
+        float percent = 0;
+
+        while(percent < 1)
+        {
+            percent += Time.deltaTime * 1 / duration;
+            timer.value = Mathf.Lerp(current, wanted, percent);
+            yield return null;
+        }
+    }
+
+}
