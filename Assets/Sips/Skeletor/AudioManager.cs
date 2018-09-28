82fc3aedafc4b00e44ee4066a747a6b989a2c015
Tuominen Joonas TTK17SP <joonastuominen@kamk.fi>
2018-09-29 00:50:54 +0300

    ADD broken enemiii

--- Assets/Sips/Skeletor/AudioManager.cs
+++ Assets/Sips/Skeletor/AudioManager.cs
@@ -10,6 +10,8 @@ public class AudioManager : MonoBehaviour
     private static List<AudioClip> SFXClips = new List<AudioClip>();
     private static List<AudioClip> MusicClips = new List<AudioClip>();
     private static VolumeChangerDelegate soundDelegate;
+    public static AudioSource MusicSource;
+    public static AudioSource SFXSource;
      
     //list of sounds and their id
 
@@ -21,6 +23,7 @@ public class AudioManager : MonoBehaviour
     /* Music ID list
      0: Ambient Music
      */
+
     /// <summary>
     /// Randomizes the pitch of a clip
     /// </summary>
@@ -29,12 +32,15 @@ public class AudioManager : MonoBehaviour
     private static void RandomizeSFX(AudioClip clip, AudioSource source)
     {
         source.clip = clip;
-        // source.pitch = Random.Range(0.8f, 1.0f);
         source.Play();
     }
 
     private static void SFXMethod(AudioSource source, int id)
     {
+        if(source.isPlaying)
+        {
+            soundDelegate(source);
+        }
         RandomizeSFX(SFXClips[id], source);
     }
 
@@ -78,6 +84,17 @@ public class AudioManager : MonoBehaviour
 
     private void Awake()
     {
+        for(int i = 0; i < GetComponentsInChildren<AudioSource>().Length; i++)
+        {
+            if(i == 0)
+            {
+                SFXSource = GetComponentsInChildren<AudioSource>()[i];
+            }
+            else
+            {
+                MusicSource = GetComponentsInChildren<AudioSource>()[i];
+            }
+        }
         soundDelegate += MusicVolumeChanger;
     }
 
--- Assets/Sips/Skeletor/UpEnemy.cs
+++ Assets/Sips/Skeletor/UpEnemy.cs
@@ -2,13 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SideEnemy : MonoBehaviour
-{
+public class UpEnemy : MonoBehaviour {
+
     private Collider2D boxCollider;
+    [SerializeField] LayerMask ground;
+    private float distance = 1.0f;
     private bool canMove = false;
     private bool canStun = true;
     private float stunTimer;
     private float moveValue = 5.0f;
+    private Vector2 m_velocity;
+
+    [Space(10)]
+    public float groundLevelAlive;
+    public float groundLevelDead;
+    public float jumpSpeed;
+    internal bool m_jumping;
+    private bool descend;
 
     private void Awake()
     {
@@ -45,15 +55,19 @@ public class SideEnemy : MonoBehaviour
 
     private void Update()
     {
+        m_velocity.y = m_jumping ? m_velocity.y - 9.81f * Time.deltaTime : 0;
         Timer();
-        if (!canMove) return;
-
-        if (canStun)
-            transform.Translate(Vector3.left * Time.deltaTime * moveValue);
-        else
+       // m_jumping = Physics2D.Raycast(transform.position, -Vector3.up, distance, ground) ? true : false;
+        if (!canStun)
             return;
+        else
+        {
+            transform.position += new Vector3(0, m_velocity.y, 0) * Time.deltaTime;
+            Jump();
+        }
 
 
+        JumpDate();
 
 
         if (boxCollider.bounds.Intersects(GameManager.Instance.Player.PLcollider))
@@ -67,6 +81,28 @@ public class SideEnemy : MonoBehaviour
 
         }
 
+    }
+
+    private void JumpDate()
+    {
+        if (!m_jumping) return;
+
+        if (transform.position.y < groundLevelDead || transform.position.y > groundLevelAlive)
+        {
+            return;
+        }
+
+        m_jumping = false;
+        transform.position = new Vector3(transform.position.x, groundLevelAlive, transform.position.z);
+    }
+
+    private void Jump()
+    {
+        if (m_jumping) return;
+
+        m_velocity.y = jumpSpeed;
+        m_jumping = true;
+        transform.position += Vector3.up * 0.025f;
     }
 
     private void TakeDamage()
