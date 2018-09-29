37e26e141ceb5d64bc96a651e791fff3a0c9bb0d
Kiiskinen Mauno TTK17SP <maunokiiskinen@kamk.fi>
2018-09-29 12:12:25 +0300

    Added parallax controller and camera follow script

---
+++ Assets/Sips/_Mgl_scripts/CameraFollowPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+
+    Transform playerPos;
+
+    [SerializeField] Vector3 offset;
+    [SerializeField] float moveSpeed;
+
+    void Start()
+    {
+        playerPos = GameManager.Instance.Player.transform;
+    }
+
+    void Update()
+    {
+        float hor = Input.GetAxisRaw("Horizontal");
+
+        Vector3 wantedPosition = new Vector3(playerPos.position.x + offset.x * hor, transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, wantedPosition, moveSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Sips/_Mgl_scripts/ParallaxMagic.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxMagic : MonoBehaviour
+{
+
+    [SerializeField] GameObject[] parallaxes = new GameObject[3];
+    float parallaxWidth = 94f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+
+            transform.parent.transform.position += Vector3.right * parallaxWidth;
+
+            //for (int i = 0; i < parallaxes.Length; i++)
+            //{
+            //    parallaxes[i].transform.position += Vector3.right * parallaxWidth;
+            //}
+        }
+    }
+}
