6516be434b7f94eb3555d432501d0fc9e76115b2
Kiiskinen Mauno TTK17SP <maunokiiskinen@kamk.fi>
2018-09-29 20:46:33 +0300

    Added fixed camera movement and fixed parallax stuff

--- Assets/Sips/_Mgl_scripts/CameraFollowPlayer.cs
+++ Assets/Sips/_Mgl_scripts/CameraFollowPlayer.cs
@@ -10,16 +10,21 @@ public class CameraFollowPlayer : MonoBehaviour
     [SerializeField] Vector3 offset;
     [SerializeField] float moveSpeed;
 
+    public Vector3 currentVelocity;
+
     void Start()
     {
         playerPos = GameManager.Instance.Player.transform;
     }
 
-    void Update()
+    void FixedUpdate()
     {
         float hor = Input.GetAxisRaw("Horizontal");
 
         Vector3 wantedPosition = new Vector3(playerPos.position.x + offset.x * hor, transform.position.y, transform.position.z);
         transform.position = Vector3.Lerp(transform.position, wantedPosition, moveSpeed * Time.deltaTime);
+
+
+        //transform.position = Vector3.SmoothDamp(transform.position, wantedPosition, ref currentVelocity, moveSpeed * Time.deltaTime, moveSpeed);
     }
 }
